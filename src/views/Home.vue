097812b07afabc67425c9d6b6371e2dec2c3bcac
yangyisong <1091139789@qq.com>
2021-07-15 12:03:58 +0800

    使用ref定义数据

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,21 @@
   <nav-header></nav-header>
   <nav-main></nav-main>
   <nav-footer></nav-footer>
+  <div>{{ num }}</div>
+  <div>{{ name }}</div>
+    <div>{{ arr }}</div>
 </template>
 
 <script>
 
-import {defineComponent} from 'vue'
+import {defineComponent,ref} from 'vue'
  
 import NavFooter from '@/components/navFooter/NavFooter'
 import NavHeader from '@/components/navHeader/NavHeader.vue'
 import NavMain from '@/components/navMain/NavMain.vue'
 export default defineComponent({
+
+  
   name:'Home',
   //接收父组件数据
   props:{
@@ -24,8 +29,13 @@ export default defineComponent({
     NavFooter
   },
   setup(props,ctx){
+    let num = ref(10)
+    let name = ref('jack')
+    let arr = ref([0,1,2,3])
     return{
-
+      num,
+      name,
+      arr
     }
   }
 })
