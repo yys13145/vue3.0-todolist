e5b0fceab0aebc022c68d3e4c6c1b9360d68b2dc
yangyisong <1091139789@qq.com>
2021-07-26 11:47:44 +0800

    vuex状态管理定义

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,28 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  //定义全局所需状态
   state: {
+    name:'jack'
   },
+  //同步修改state(方法)
+  //第一个参数state,第二个参数是需要修改的值
   mutations: {
+    setName(state,payload){
+      state.name = payload
+    }
   },
+  //异步提交mutation
+  //第一个参数是store对象，第二个参数是要修改的值
   actions: {
+    asyncSetName(store,params){
+      setTimeout(()=>{
+        //commit是提交调用mutation的方法
+        store.commit('setName',params)
+      },2000)
+    }
   },
+  //模块化
   modules: {
   }
 })
