85f562c8a46e6b8b4eb2a02599e55aebdbd05b6d
yangyisong <1091139789@qq.com>
2021-07-26 12:00:55 +0800

    定义todolist状态管理方法

--- src/store/index.js
+++ src/store/index.js
@@ -3,24 +3,39 @@ import { createStore } from 'vuex'
 export default createStore({
   //定义全局所需状态
   state: {
-    name:'jack'
+    list:[
+      {
+        title:'吃饭',
+        complete:false
+      },
+      {
+        title:'睡觉',
+        complete:false
+      },
+      {
+        title:'打豆豆',
+        complete:true
+      }
+    ]
   },
   //同步修改state(方法)
   //第一个参数state,第二个参数是需要修改的值
   mutations: {
-    setName(state,payload){
-      state.name = payload
+    addTodo(state,data){
+      state.list.push(data)
+    },
+    deleteTodo(state,index){
+      state.list.splice(index,1)
+    },
+    //把过滤后的数组传过来
+    clearComplete(state,arr){
+      state.list = arr
     }
   },
   //异步提交mutation
   //第一个参数是store对象，第二个参数是要修改的值
   actions: {
-    asyncSetName(store,params){
-      setTimeout(()=>{
-        //commit是提交调用mutation的方法
-        store.commit('setName',params)
-      },2000)
-    }
+    
   },
   //模块化
   modules: {
